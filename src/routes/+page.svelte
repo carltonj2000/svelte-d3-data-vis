f8fb04e91a384e55f510ea9e4367f6ff64fa0fa4
Carlton Joseph <carlton.joseph@gmail.com>
2023-01-18 22:00:28 -0800

    basic svg added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import students from '../data/data.js';
+	import { scaleLinear } from 'd3-scale';
+	import { max } from 'd3-array';
+
+	let width = 400;
+	let height = 400;
+
+	const xScale = scaleLinear().domain([0, 110]).range([0, width]);
+	const yScale = scaleLinear()
+		.domain([0, max(students, (s) => s.hours) + 10])
+		.range([height, 0]);
+</script>
+
+<svg {width} {height}>
+	{#each students as { grade, hours }}
+		<circle cx={xScale(grade)} cy={yScale(hours)} r="10" fill="purple" stroke="yellow" />
+	{/each}
+</svg>
+
